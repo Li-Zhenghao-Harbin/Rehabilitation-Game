b77499bb743182a31cd1844bb5cbbc4054fcc147
Li-Zhenghao-Harbin <a15846593095@126.com>
2023-02-26 14:05:37 +0000

    BGM

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+	public GameObject Bgm;
+	GameObject BGM;
+
+	// Use this for initialization
+	void Start () {
+		BGM = GameObject.FindGameObjectWithTag("sound");
+		if (BGM == null)
+        {
+			BGM = (GameObject)Instantiate(Bgm);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
